45488fa38ca88d1eea1da92d7fafaaaa5aeb944f
Saatvik Shukla <me@saatvikshukla.com>
2017-12-02 13:02:29 +0530

    Update __init__
    - Add Ajax functionality
    - Add iw commands to handle wifi

--- webModule/web.py
+++ webModule/web.py
@@ -4,12 +4,33 @@
 # Saatvik Shukla
 # Dec 2 2017, 9.51am
 
-from flask import Flask
+import commands
+import subprocess
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/update')
+def add_numbers():
+	try:
+		status, res = commands.getstatusoutput("iw wlp2s0 scan | grep 'abc' -B 4 -A 5 | egrep 'SSID|signal|last seen'")
+		print(status)
+		print(res)
+	except Exception as e:
+		raise e
+		print("Exception."+e)
+
+	if status == 0:
+		print "Scanning success."
+	elif status == 1:
+		res=None
+		print("Error while scanning")
+
+	return jsonify(result=res)
+
 
 if __name__ == "__main__":
     app.run()
