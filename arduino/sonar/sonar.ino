a8a4b1a03317db3c3aecc3ff7e375d678e44d388
Saatvik Shukla <me@saatvikshukla.com>
2017-12-03 03:33:56 +0530

    Update
    - Update sonar code with buzzer
    - Update Py

--- arduino/sonar/sonar.ino
+++ arduino/sonar/sonar.ino
@@ -1,7 +1,10 @@
-#include <Servo.h>
+// Includes the Servo library
+#include<Servo.h>
 // Defines Tirg and Echo pins of the Ultrasonic Sensor
 const int trigPin = 11;
 const int echoPin = 10;
+int const buzzPin = 2;
+
 // Variables for the duration and the distance
 long duration;
 int distance;
@@ -9,20 +12,21 @@ Servo myServo; // Creates a servo object for controlling the servo motor
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(buzzPin, OUTPUT);
   Serial.begin(9600);
-  myServo.attach(12); // Defines on which pin is the servo motor attached
+  myServo.attach(6); // Defines on which pin is the servo motor attached
 }
 void loop() {
   // rotates the servo motor from 15 to 165 degrees
   for(int i=30;i<=150;i++){  
   myServo.write(i);
   delay(30);
-  distance = calculateDistance();
+  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
   
-  //Serial.print(i); // Sends the current degree into the Serial Port
-  //Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   Serial.print(distance); // Sends the distance value into the Serial Port
-  //Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
   // Repeats the previous lines from 165 to 15 degrees
   for(int i=150;i>30;i--){  
@@ -37,7 +41,7 @@ void loop() {
 }
 // Function for calculating the distance measured by the Ultrasonic sensor
 int calculateDistance(){ 
-  
+  int d2;
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -46,5 +50,17 @@ int calculateDistance(){
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
   distance= duration*0.034/2;
+
+  d2 = (duration/2) / 29.1;
+
+  // if distance less than 0.5 meter and more than 0 (0 or less means over range)
+  if (d2 <= 150 && d2 >= 0) {
+    digitalWrite(buzzPin, HIGH);
+    delay(10);
+    digitalWrite(buzzPin, LOW);
+  } else {
+  // Don't buzz
+    digitalWrite(buzzPin, LOW);
+  }
   return distance;
 }
--- webModule/web.py
+++ webModule/web.py
@@ -4,6 +4,7 @@
 # Saatvik Shukla
 # Dec 2 2017, 9.51am
 
+import math
 import commands
 import subprocess
 from flask import Flask, render_template, request, jsonify
@@ -13,52 +14,51 @@ app = Flask(__name__)
 def hello(name=None):
     return render_template('index2.html')
 
+def average(n1,n2):
+	return ((n1+n2)/2)
+
 @app.route('/update')
 def update():
 	try:
-		
-		status, res = commands.getstatusoutput("iw wlp2s0 scan | grep 'black34' -B 3 | sed -n '1p;4p'")
-		# status, res = commands.getstatusoutput("iwlist wlp2s0 scanning | grep 'RONAK' -B 3 -A 3 | egrep 'ESSID|Quality'")
-		# status, res = commands.getstatusoutput("iw wlp2s0 scan | grep 'RONAK' -B 4 -A 5 | egrep 'SSID|signal|last seen'")
+		pass
 	except Exception as e:
 		raise e
 		res = 'Error Scanning networks.'
 		print("Exception."+e)
 
-	if status == 0:
-		try:
-			alpha = res.split('\n\t')
-			alpha[1] = (alpha[1].strip());
-			alpha[0] = alpha[0].replace("\tsignal: -","")
-			alpha[0] = alpha[0].replace(".00 dBm","")
-			alpha[1] = alpha[1].replace("SSID: ","")
-		except Exception as e:
-			print("Exception."+e)
-			alpha = 'Error parsing scanned networks.'
-			raise e
-	else:
-		alpha = 'Error parsing scanned networks.'
+	try:
+		cmd = "iw wlan0 scan | grep 'OP' -B 9 | egrep 'freq:|signal|SSID:' | sed -n '1p;2p;3p'"
+		status, res = commands.getstatusoutput(cmd)
+		alpha = res.split('\n\t')
+		alpha[1] = (alpha[1].strip());
+		alpha[0] = alpha[0].replace("\tfreq: ","")
+		alpha[1] = alpha[1].replace("signal: -","")
+		alpha[1] = alpha[1].replace(".00 dBm","")
+		alpha[2] = alpha[2].replace("SSID: ","")
 
-	# if (alpha[0] >= 80):
-	# 	alpha.append("<style>.demo-ribbon{background-color:#518946} .demo-card-square.mdl-card {display:block} .demo-card-square > .mdl-card__title {background:url('') bottom right 15% no-repeat #518946;}</style>")
-	# 	alpha.append("Everything seems clear")
-	# 	alpha.append("It seems to be clear around you, keep driving.")
-	# elif (alpha[0] < 80 and alpha[0] > 60): ###
-	# 	alpha.append("<style>.demo-ribbon{background-color:#E4553D} .demo-card-square.mdl-card {display:block} .demo-card-square > .mdl-card__title {background:url('') bottom right 15% no-repeat #E4553D;}</style>")
-	# 	alpha.append("A car is closeby")
-	# 	alpha.append("There is a car nearby.")
-	# elif (alpha[0] < 60 and alpha > 45):
-	# 	alpha.append("<style>.demo-ribbon{background-color:#C44335} .demo-card-square.mdl-card {display:block} .demo-card-square > .mdl-card__title {background:url('') bottom right 15% no-repeat #C44335;}</style>")
-	# 	alpha.append("Vehicle Alert")
-	# 	alpha.append("A vehicle is close to you, beware.")		
-	# elif (alpha[0] <= 45 ):
-	# 	alpha.append("<style>.demo-ribbon{background-color:#C44335} .demo-card-square.mdl-card {display:block} .demo-card-square > .mdl-card__title {background:url('') bottom right 15% no-repeat #C44335;}</style>")
-	# 	alpha.append("Vehicle Alert")
-	# 	alpha.append("A vehicle is close to you, beware.")
+		status, res = commands.getstatusoutput(cmd)
+		beta = res.split('\n\t')
+		beta[1] = (beta[1].strip());			
+		beta[0] = beta[0].replace("\tfreq:: ","")
+		beta[1] = beta[1].replace("signal: -","")
+		beta[1] = beta[1].replace(".00 dBm","")
+		beta[2] = beta[2].replace("SSID: ","")
+
+	except Exception as parsingError:
+		print("Exception."+parsingError)
+		alpha = 'Error parsing scanned networks.'
+		beta = 'Error parsing scanned networks.'
+		gamma = 'Error parsing scanned networks.'
+		theta = 'Error parsing scanned networks.'
+		zeta = 'Error parsing scanned networks.'
+		raise parsingError
+
+	temp = average(int(alpha[1]), int(beta[1]))
+	alpha[1] = temp
 	print(alpha)
-	print(status)
-	# print(res)
 	return jsonify(result=alpha)
+
+
 
 
 if __name__ == "__main__":
